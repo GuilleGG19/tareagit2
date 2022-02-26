85f3fe15adc2e1d6a52e66def680dca7cfafcc45
PROGRAMACION <jgutierrrezg@miumg.edu.gt>
2022-02-25 21:31:47 -0600

    cam1

---
+++ entrada_y_salida/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace entrada_y_salida
+{
+    class EntradaSalida
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
---
+++ leerTeclado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeerTeclado
+{
+    /// <summary>
+    /// leer datos desde teclado
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos numeros");
+            Console.Write("Ingresa numero 1: ");
+            // Los datos se leen como cadena
+            string dato = Console.ReadLine();
+            // Convertir a numero con int.Parse
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingresa numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+        }
+    }
+}
